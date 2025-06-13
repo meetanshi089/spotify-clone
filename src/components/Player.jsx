fa6e01b22d15508a1bddc36778d89facd27537c7
meetanshi089 <meetanshitomar@gmail.com>
2025-06-13 14:57:42 +0530

    components added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -21,6 +21,23 @@ const Player = () => {
           <img className="w-4 cursor-pointer" src={assets.next_icon} />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} />
         </div>
+        <div className="flex items-center gap-5">
+          <p>1:06</p>
+          <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+            <hr className="h-1 border-none w-0 bg-green-800 rounded-full"></hr>
+          </div>
+          <p>3:02</p>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4" src={assets.plays_icon} alt="" />
+        <img className="w-4" src={assets.mic_icon} alt="" />
+        <img className="w-4" src={assets.queue_icon} alt="" />
+        <img className="w-4" src={assets.speaker_icon} alt="" />
+        <img className="w-4" src={assets.volume_icon} alt="" />
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
+        <img className="w-4" src={assets.mini_player} alt="" />
+        <img className="w-4" src={assets.zoom_icon} alt="" />
       </div>
     </div>
   );
